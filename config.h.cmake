6b8491d3e8994ae407be98b96276cb5ce691e7ad
Peter Drahoš <drahosp@gmail.com>
2010-10-01 03:28:39 +0200

    Import to git

---
+++ config.h.cmake
@@ -0,0 +1,203 @@
+/* include/IL/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Altivec extension found */
+#cmakedefine ALTIVEC_GCC
+
+/* PPC_ASM assembly found */
+#cmakedefine GCC_PCC_ASM
+
+/* X86_64_ASM assembly found */
+#cmakedefine GCC_X86_64_ASM
+
+/* X86_ASM assembly found */
+#cmakedefine GCC_X86_ASM
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#cmakedefine HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#cmakedefine HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#cmakedefine HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#cmakedefine HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#cmakedefine HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#cmakedefine HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#cmakedefine HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#cmakedefine HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#cmakedefine HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#cmakedefine HAVE_UNISTD_H 1
+
+/* Support Allegro API */
+#cmakedefine ILUT_USE_ALLEGRO
+
+/* Support DirectX8 API */
+#cmakedefine ILUT_USE_DIRECTX8
+
+/* Support DirectX9 API */
+#cmakedefine ILUT_USE_DIRECTX9
+
+/* Support OpenGL API */
+#cmakedefine ILUT_USE_OPENGL
+
+/* Support SDL API */
+#cmakedefine ILUT_USE_SDL
+
+/* BMP support */
+#undef IL_NO_BMP
+
+/* DCX support */
+#undef IL_NO_DCX
+
+/* GIF support */
+#undef IL_NO_GIF
+
+/* HDR support */
+#undef IL_NO_HDR
+
+/* ICON support */
+#undef IL_NO_ICON
+
+/* JPG support */
+#undef IL_NO_JPG
+
+/* LCMS support */
+#undef IL_NO_LCMS
+
+/* LIF support */
+#undef IL_NO_LIF
+
+/* MDL support */
+#undef IL_NO_MDL
+
+/* MNG support */
+#undef IL_NO_MNG
+
+/* PCD support */
+#undef IL_NO_PCD
+
+/* PCX support */
+#undef IL_NO_PCX
+
+/* PIC support */
+#undef IL_NO_PIC
+
+/* PIX support */
+#undef IL_NO_PIX
+
+/* PNG support */
+#undef IL_NO_PNG
+
+/* PNM support */
+#undef IL_NO_PNM
+
+/* PSD support */
+#undef IL_NO_PSD
+
+/* PSP support */
+#undef IL_NO_PSP
+
+/* PXR support */
+#undef IL_NO_PXR
+
+/* RAW support */
+#undef IL_NO_RAW
+
+/* SGI support */
+#undef IL_NO_SGI
+
+/* TGA support */
+#undef IL_NO_TGA
+
+/* TIF support */
+#undef IL_NO_TIF
+
+/* WAD support */
+#undef IL_NO_WAD
+
+/* WAL support */
+#undef IL_NO_WAL
+
+/* XPM support */
+#undef IL_NO_XPM
+
+/* Use libjpeg without modification. always enabled. */
+#cmakedefine IL_USE_JPEGLIB_UNMODIFIED
+
+/* LCMS include without lcms/ support */
+#cmakedefine LCMS_NODIRINCLUDE
+
+/* Building on Mac OS X */
+#cmakedefine MAX_OS_X
+
+/* memalign memory allocation */
+#cmakedefine MEMALIGN
+
+/* mm_malloc memory allocation */
+#cmakedefine MM_MALLOC
+
+/* Name of package */
+#define PACKAGE "${PACKAGE}"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "${PACKAGE_BUGREPORT}"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "${PACKAGE_NAME}"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "${PACKAGE_STRING}"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "${PACKAGE_TARNAME}"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "${PACKAGE_VERSION}"
+
+/* posix_memalign memory allocation */
+#cmakedefine POSIX_MEMALIGN
+
+/* SSE extension found */
+#cmakedefine SSE
+
+/* SSE2 extension found */
+#cmakedefine SSE2
+
+/* SSE3 extension found */
+#cmakedefine SSE3
+
+/* Define to 1 if you have the ANSI C header files. */
+#cmakedefine STDC_HEADERS
+
+/* valloc memory allocation */
+#cmakedefine VALLOC
+
+/* Memory must be vector aligned */
+#cmakedefine VECTORMEM
+
+/* Version number of package */
+#cmakedefine VERSION
+
+/* Define to 1 if your processor stores words with the most significant byte
+   first (like Motorola and SPARC, unlike Intel and VAX). */
+#cmakedefine WORDS_BIGENDIAN
+
+/* Define to 1 if the X Window System is missing or not being used. */
+#cmakedefine X_DISPLAY_MISSING
+
+
+
